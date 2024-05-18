0eac7720519e6f92884afa308efc3ba135886844
RadwanHegazy <radwangaber22@gmail.com>
2024-05-18 20:50:35 +0300

    configure updates

--- tenant/apis/urls.py
+++ tenant/apis/urls.py
@@ -1,7 +1,9 @@
-from .views import home, update
+from .views import home, update, get
 from django.urls import path
 
 urlpatterns = [
     path('',home.home_view.as_view()),
     path('update/',update.update_view.as_view()),
+    path('get/tenant/<str:tenant_name>/',get.tenant_view.as_view()),
+    path('get/all/',get.get_all_tenants.as_view()),
 ]
